2a911d37d16c1f9a859be406c9d51c5f1a80c929
Anuoluwa <anuoluwapop3@gmail.com>
2025-07-13 19:29:58 +0200

    add basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# define vairable 
+number1 = 10
+number2 = 5
+
+#perform operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#print results
+print("Sum of", number1, "and", number2, "is", sum_result)
+print("Difference of", number1, "and", number2, "is", difference_result)
+print("Product of", number1, "and", number2, "is", product_result)
